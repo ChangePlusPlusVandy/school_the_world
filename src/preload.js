cd088197d590789de6c02a5915fa2b791d9dc6c5
nirmalalla <allanirmal@gmail.com>
2024-10-27 21:29:12 -0500

    switching to expo

--- src/preload.js
+++
@@ -1,9 +0,0 @@
-// See the Electron documentation for details on how to use preload scripts:
-// https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
-const { contextBridge, ipcRenderer} = require("electron")
-const { createProgram, getAllPrograms } = require("./Backend/programs")
-
-contextBridge.exposeInMainWorld("electronAPI", {
-    createProgram: (doc) => ipcRenderer.invoke("createProgram", doc),
-    getAllPrograms: () => ipcRenderer.invoke("getAllPrograms"),
-})
