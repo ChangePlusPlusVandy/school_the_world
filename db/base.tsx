555cb19c04ce51c2c0d5f8ad1ae381e89bb862dd
jhdaws <ethopause902@gmail.com>
2024-11-07 14:58:03 -0600

    test cases passing

--- db/base.tsx
+++ db/base.tsx
@@ -1,55 +1,68 @@
-import * as SQLite from "expo-sqlite"
-import { insertCountry, getCountryById } from "./country";
+import * as SQLite from "expo-sqlite";
+import { insertCountry, getCountryById, editCountry } from "./country";
 
-export interface Country{
-    id: number,
-    name: String
+export interface Country {
+  id: number;
+  name: String;
 }
 
 export class DatabaseService {
-    private db: SQLite.SQLiteDatabase | null = null;
+  private db: SQLite.SQLiteDatabase | null = null;
 
-    async initDatabase(): Promise<void> {
-        try {
-            this.db = await SQLite.openDatabaseAsync("local.db");
+  async initDatabase(): Promise<void> {
+    try {
+      this.db = await SQLite.openDatabaseAsync("local.db");
 
-
-            await this.db.execAsync(`
+      await this.db.execAsync(`
                 CREATE TABLE IF NOT EXISTS countries(
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 name TEXT NOT NULL
                 )
             `);
 
-            console.log("db initialized successfully");
-        } catch (err) {
-            console.error("error initializing db: ", err)
-        }
+      console.log("db initialized successfully");
+    } catch (err) {
+      console.error("error initializing db: ", err);
     }
- 
-    async addCountry(countryName: string): Promise<Country|null> {
-        try {
-            if (!this.db) throw new Error("Database not initialized");
-            return await insertCountry(this.db, countryName, this.getCountryById.bind(this));
-        } catch (err) {
-            console.error("error adding country: ", err);
-            return null
-        }
-    }
+  }
 
-    async getCountryById(id: number): Promise<Country|null> {
-        try {
-            if (!this.db) throw new Error("db not initialized");
-            return await getCountryById(this.db, id)
-        } catch (err) {
-            console.error("error getting country by id", err);
-            return null
-        }
+  async addCountry(countryName: string): Promise<Country | null> {
+    try {
+      if (!this.db) throw new Error("Database not initialized");
+      return await insertCountry(
+        this.db,
+        countryName,
+        this.getCountryById.bind(this)
+      );
+    } catch (err) {
+      console.error("error adding country: ", err);
+      return null;
     }
+  }
+
+  async getCountryById(id: number): Promise<Country | null> {
+    try {
+      if (!this.db) throw new Error("db not initialized");
+      return await getCountryById(this.db, id);
+    } catch (err) {
+      console.error("error getting country by id", err);
+      return null;
+    }
+  }
+
+  async editCountry(id: number, countryName: string): Promise<Country | null> {
+    try {
+      if (!this.db) throw new Error("db not initialized");
+      return await editCountry(this.db, id, countryName);
+    } catch (err) {
+      console.error("error editing country: ", err);
+      return null;
+    }
+  }
 }
 
 export const createDatabase = async () => {
-    const dbService = new DatabaseService();
-    await dbService.initDatabase();
-    return dbService
-}
+  const dbService = new DatabaseService();
+  await dbService.initDatabase();
+  return dbService;
+};
