356048406dd51a11f47f0389ea045e2bfa8f5384
JihengLi <jiheng.li.1@vanderbilt.edu>
2025-03-27 10:21:49 -0500

    finish style change

--- app/choose_annual_filtered.tsx
+++ app/choose_annual_filtered.tsx
@@ -1,94 +1,91 @@
-import { View, Text, TouchableOpacity, Button, StyleSheet} from "react-native";
-import { Link } from 'expo-router';
-import { MaterialIcons} from "@expo/vector-icons";
+import { View, Text, TouchableOpacity, Button, StyleSheet } from "react-native";
+import { Link } from "expo-router";
+import { MaterialIcons } from "@expo/vector-icons";
 
+export default function Choose_Annual_Filtered() {
+  return (
+    <View style={styles.container}>
+      {/* top line */}
+      <View style={styles.topBar}>
+        <TouchableOpacity
+          onPress={() => {}}
+          style={{ position: "absolute", left: 10 }}
+        >
+          <MaterialIcons name="chevron-left" size={30} color="darkblue" />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => {}}>
+          <MaterialIcons name="home" size={32} color="darkblue" />
+        </TouchableOpacity>
+      </View>
 
-export default function Choose_Annual_Filtered () {
-    return (
-        <View style={styles.container}>
-            {/* top line */}
-            <View style={styles.home_back_row}>
-                <MaterialIcons name="arrow-back" size={30} style={{paddingRight: 110, paddingLeft: 40, paddingTop: 10}}/>
-                <MaterialIcons name="home-filled" style={{marginBottom: 20}} size={50}/>
-            </View>
+      {/* title section */}
+      <Text style={styles.title}>Choose Type</Text>
 
-            {/* title section */}
-            <View style={styles.titleContainer}>
-                <Text style={styles.titleText}>Choose Type</Text>
-            </View>
+      {/* Annual data link */}
+      <View style={styles.box1Container}>
+        <Link href="/choose_annual_filtered" style={styles.box1Text}>
+          Annual Data
+        </Link>
+      </View>
 
-            {/* Annual data link */}
-            <View style={styles.box1Container}>
-                <Link href="/choose_annual_filtered" style={styles.box1Text}>Annual Data</Link>
-            </View>
-
-            {/* Filtered data link */}
-            <View style={styles.box1Container}>
-            <Link href="/choose_annual_filtered" style={styles.box1Text}>Filtered Data</Link>
-            </View>
-        </View>
-    );
+      {/* Filtered data link */}
+      <View style={styles.box1Container}>
+        <Link href="/choose_annual_filtered" style={styles.box1Text}>
+          Filtered Data
+        </Link>
+      </View>
+    </View>
+  );
 }
 
-
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: 'center',
-      backgroundColor: '#EFF2F7'
+  container: {
+    flex: 1,
+    backgroundColor: "#f5f7fa",
+    alignItems: "center",
+    paddingTop: 60,
+    paddingHorizontal: 20,
+  },
+  topBar: {
+    justifyContent: "center",
+    alignItems: "center",
+    position: "relative",
+    flexDirection: "row",
+    width: "100%",
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: "bold",
+    marginTop: 24,
+    marginBottom: 32,
+    paddingBottom: "3%",
+  },
+  box1Text: {
+    fontSize: 20,
+    fontWeight: 700,
+    color: "#000",
+    fontStyle: "normal",
+    letterSpacing: 0.75,
+    fontFamily: "Plus Jakarta Sans",
+  },
+  box1Container: {
+    backgroundColor: "white",
+    borderRadius: 16,
+    padding: 20,
+    width: "85%",
+    marginBottom: 16,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
     },
-
-    home_back_row: {
-        flexDirection: 'row',
-        justifyContent: 'flex-start',
-        // backgroundColor: 'red',
-        width: 400
-    },
-
-    backButton: {
-      width: 42,
-      height: 42
-    },
-
-    titleText: {
-        fontSize: 25,
-        fontWeight: 700,
-        color: '#000',
-        fontStyle: 'normal',
-        letterSpacing: 0.75,
-        fontFamily: 'Plus Jakarta Sans'
-    },
-
-    titleContainer: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginBottom: 100,
-        marginTop: 40
-    },
-
-    box1Text: {
-        fontSize: 20,
-        fontWeight: 700,
-        color: '#000',
-        fontStyle: 'normal',
-        letterSpacing: 0.75,
-        fontFamily: 'Plus Jakarta Sans',
-    },
-
-    box1Container: {
-        backgroundColor: '#fff',
-        width: 331,
-        height: 102,
-        borderStyle: 'solid',
-        borderColor: '#000',
-        borderRadius: 20,
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginBottom: 20
-    },
-
-    topLineContainer: {
-        justifyContent: 'flex-start',
-        alignItems: 'flex-start'
-    }   
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 3,
+    alignItems: "center",
+  },
+  topLineContainer: {
+    justifyContent: "flex-start",
+    alignItems: "flex-start",
+  },
 });
